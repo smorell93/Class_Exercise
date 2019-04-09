3fd3dbbf5e036df64eade104fea879c1e00259b3
Sara Morell <smorell93@gmail.com>
2019-04-09 11:20:30 -0400

    updating prog

--- prog.py
+++ prog.py
@@ -1,7 +1,11 @@
+#import relevant functions
 import random
+import json
 
+#Create a list of vowels
 vowels = 'a' or 'e' or 'i' or 'o' or 'u'
 
+#Create function to count vowels
 def num_vowels(string):
     count = 0
     for i in string:
@@ -9,4 +13,5 @@ def num_vowels(string):
             count += 1
     print(count)
 
+#Test Function
 num_vowels("Sara")
